e06580ba0c019a311048afb1061ab6ad360e6ec3
clara_r <crenauro@optimum.net>
2021-02-27 20:37:28 -0500

    readME now generates, badges dont

---
+++ util/README.md
@@ -0,0 +1,34 @@
+
+ ## TITLE 
+ nd
+
+ ## DESCRIPTION
+ d
+
+ ## TABLE OF CONTENTS
+ Installation
+
+ ## INSTALLATION
+ d
+
+ ## USAGE
+ d
+
+ ## LICENSE
+ Apache
+ undefined
+ undefined
+ undefined
+
+ ## CONTRIBUTE
+ d
+
+ ## TESTS
+ d
+
+ ## GITHUB
+ d
+
+ ## EMAIL
+ d
+ 
--- util/generatemarkdown.js
+++ util/generatemarkdown.js
@@ -6,21 +6,21 @@ const fs = require('fs');
 
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    if (license == "Apache") {
+function renderLicenseBadge(choices) {
+    if (choices == "Apache") {
         renderLicenseBadge('https://img.shields.io/badge/License-Apache%202.0-blue.svg');
-      } else if (license == "GNU") {
+      } else if (choices == "GNU") {
         renderLicenseBadge('https://img.shields.io/badge/License-GPLv3-blue.svg');
-      } else if (license == "MIT") {
+      } else if (choices == "MIT") {
         renderLicenseBadge('https://img.shields.io/badge/License-MIT-yellow.svg');
-      } else if (license == "ISC") {
+      } else if (choices == "ISC") {
         renderLicenseBadge('https://img.shields.io/badge/License-ISC-blue.svg');
       } else return ("");
     };
 
 // Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
+function renderLicenseLink(choices) {
     if (choices == "Apache") {
         renderLicenseLink('https://opensource.org/licenses/Apache-2.0');
       } else if (choices == "GNU") {
@@ -35,7 +35,7 @@ function renderLicenseLink(license) {
 
 // Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+function renderLicenseSection(choices) {
     if (choices == "Apache") {
         renderLicenseSection('Apache');
       } else if (choices == "GNU") {
@@ -103,21 +103,78 @@ const questions = [
         },
     ];   
 
-// Function to write the readme
+// // Function to write the readme
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 };
 
 
-//function to initialize the application
+// //function to initialize the application
+// function generateMarkdown() {
+//     inquirer.prompt(questions).then((inquirerResponses) => {
+//       console.log("generating your readME");
+//     writeToFile('README.md', generateMarkdown({...inquirerResponses}))
+//   }) .catch(function (err){
+//   console.log(err);
+
+// });
+// }
+
+// generateMarkdown();
+
+
+// function generateMarkdown() {
+//   inquirer.prompt(questions).then((inquirerResponses) => {
+//     console.log("generating your readME");
+//   writeToFile('README.md', `YOUR RESPONSES ARE: ${inquirerResponses}`)
+// }) .catch(function (err){
+// console.log(err);
+
+// });
+// }
+
+// generateMarkdown();
+
 function generateMarkdown() {
-    inquirer.prompt(questions).then((inquirerResponses) => {
-      console.log("generating your readME");
-    writeToFile('README.md', gernerateMarkdown({...inquirerResponses}))
-  }) .catch(function (err){
-  console.log(err);
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("generating your readME");
+    var myString = `
+ ## TITLE 
+ ${inquirerResponses.title}
 
-});
-}
+ ## DESCRIPTION
+ ${inquirerResponses.description}
 
-generateMarkdown();
+ ## TABLE OF CONTENTS
+ ${inquirerResponses.toc}
+
+ ## INSTALLATION
+ ${inquirerResponses.installation}
+
+ ## USAGE
+ ${inquirerResponses.usage}
+
+ ## LICENSE
+ ${inquirerResponses.license}
+ ${inquirerResponses.renderLicenseBadge}
+ ${inquirerResponses.renderLicenseLink}
+ ${inquirerResponses.renderLicenseSection}
+
+ ## CONTRIBUTE
+ ${inquirerResponses.contribute}
+
+ ## TESTS
+ ${inquirerResponses.tests}
+
+ ## GITHUB
+ ${inquirerResponses.github}
+
+ ## EMAIL
+ ${inquirerResponses.email}
+ `
+    writeToFile('README.md', myString)
+ }) .catch(function (err){
+ console.log(err);
+ });
+ }
+ generateMarkdown();
